fcdc76922655bf7e82bca39548794eccda162ef2
Cameron Schaus <cam@schaus.ca>
2025-05-23 09:56:08 -0600

    rework connector around DAO pattern

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 'use strict';
 
-exports.initialize = function initializeDataSource(dataSource, callback) {
-  const Voipms = require('./lib/voipms');
-  const settings = dataSource.settings || {};
-
-  dataSource.connector = new Voipms(settings);
-  dataSource.connector.dataSource = dataSource;
-
-  callback && callback();
-};
+module.exports = require('./lib/voipms');
